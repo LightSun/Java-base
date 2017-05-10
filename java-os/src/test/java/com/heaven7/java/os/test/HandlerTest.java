4cbd9f84302f06fd81f09f0b55d003d0e363b1a7
LinghtSun <532278976@qq.com>
2017-05-11 01:49:52 +0800

    m

--- java-os/src/test/java/com/heaven7/java/os/test/HandlerTest.java
+++ java-os/src/test/java/com/heaven7/java/os/test/HandlerTest.java
@@ -1,6 +1,7 @@
 package com.heaven7.java.os.test;
 
 import com.heaven7.java.os.Handler;
+import com.heaven7.java.os.HandlerThread;
 import com.heaven7.java.os.Looper;
 import com.heaven7.java.os.Message;
 
@@ -8,13 +9,31 @@ import junit.framework.TestCase;
 
 public class HandlerTest extends TestCase{
 
+	private final Object mLock = new Object();
 	private Handler mHandler;
-	private Object mLock = new Object();
 	private Thread mThread;
+	private HandlerThread mHt;
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		//initChildThread();
+		initHandlerThread();
+	}
+
+	private void initHandlerThread() {
+		mHt = new HandlerThread("ht");
+		mHt.start();
+		mHandler = new Handler(mHt.getLooper(), new Handler.Callback() {
+			@Override
+			public boolean handleMessage(Message msg) {
+				System.out.println("HandlerTest ok: " + msg);
+				return false;
+			}
+		});
+	}
+
+	private void initChildThread() throws InterruptedException  {
 		mThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -39,6 +58,9 @@ public class HandlerTest extends TestCase{
 		synchronized (mLock) {
 			mLock.wait();
 		}
+	}
+	
+	private void testMainLooper(){
 		//main thread bug. made block for ever...
 		/*Looper.prepareMainLooper();
 		Handler = new Handler(Looper.getMainLooper(), new Handler.Callback() {
