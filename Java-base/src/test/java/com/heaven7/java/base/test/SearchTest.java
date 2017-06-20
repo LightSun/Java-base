a8e351c37bb74b1f023a68a4dac7b224c113f1e6
LightSun <532278976@qq.com>
2017-06-20 10:57:12 +0800

    no

--- Java-base/src/test/java/com/heaven7/java/base/test/SearchTest.java
+++ Java-base/src/test/java/com/heaven7/java/base/test/SearchTest.java
@@ -10,47 +10,7 @@ import junit.framework.TestCase;
 
 public class SearchTest extends TestCase{
 	
-	public static void main(String[] args) {
-		/*System.out.println(0xff );             //255
-		System.out.println( (0xff + 5) >> 8 ); //1
-		System.out.println( (0xff + 1) >> 8 ); //1
-		System.out.println(Math.pow(2, 8));    //256
-		System.out.println(((int)Math.pow(2, 8)) >> 8); //1
-*/		
-		//System.out.println(Integer.MAX_VALUE);
-		//System.out.println(Long.MAX_VALUE);
-		//System.out.println(5 + 0x10000000);
-		int val = 5 +          0x10000000 ;
-		//高位的值
-		/*System.out.println((val & 0xf0000000) == 0x10000000);
-		System.out.println( val & 0x0fffffff);
-		System.out.println(5);
-		System.out.println(0x5ffff >> 16);
-		System.out.println(0x15ffff >> 16);
-		System.out.println(0x8ffff >> 16);
-		System.out.println(0xffff);
-		System.out.println(0xfff >> 16);
-		System.out.println( 0x01ffffff );
-		System.out.println(1 << 24);*/
-		
-		int baseKey = 5 ;
-		int count = 10 << 16;
-		int tag = 3 << 24;
-		int key = baseKey + count + tag;
-		
-		key = 7;
-		System.out.println("count = " + count);
-		System.out.println("tag = " + tag);
-		System.out.println(key & 0x0000ffff);
-		System.out.println( (key & 0x00ff0000) >> 16);
-		System.out.println( (key & 0xff000000) >> 24);
-		System.out.println( ( 0xff));
-		
-		List<Integer> list = new ArrayList<>();
-		list.add(1);
-		list.add(2);
-		System.out.println();
-	}
+	
 
 	
 	public void testEmpty(){
