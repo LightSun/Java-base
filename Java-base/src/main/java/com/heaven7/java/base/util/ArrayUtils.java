dadbadb948205429e622238dd5639ab0d4c4125c
lightsun <978136772@qq.com>
2019-05-23 20:33:39 +0800

    opt

--- Java-base/src/main/java/com/heaven7/java/base/util/ArrayUtils.java
+++ Java-base/src/main/java/com/heaven7/java/base/util/ArrayUtils.java
@@ -95,6 +95,141 @@ public final class ArrayUtils {
 		return arr;
 	}
 	/**
+	 * convert the collection to byte array.
+	 * @param coll the byte collection
+	 * @return the byte array.
+	 * @since 1.2.0
+	 */
+	public static byte[] toByteArray(Collection<Byte> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final byte[] arr = new byte[coll.size()];
+		final Iterator<Byte> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			arr[i++] = it.next();
+		}
+		return arr;
+	}
+	/**
+	 * convert the collection to short array.
+	 * @param coll the short collection
+	 * @return the short array.
+	 * @since 1.2.0
+	 */
+	public static short[] toShortArray(Collection<Short> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final short[] arr = new short[coll.size()];
+		final Iterator<Short> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			arr[i++] = it.next();
+		}
+		return arr;
+	}
+	/**
+	 * convert the collection to long array.
+	 * @param coll the long collection
+	 * @return the long array.
+	 * @since 1.2.0
+	 */
+	public static long[] toLongArray(Collection<Long> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final long[] arr = new long[coll.size()];
+		final Iterator<Long> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			arr[i++] = it.next();
+		}
+		return arr;
+	}
+	/**
+	 * convert the collection to float array.
+	 * @param coll the float collection
+	 * @return the float array.
+	 * @since 1.2.0
+	 */
+	public static float[] toFloatArray(Collection<Float> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final float[] arr = new float[coll.size()];
+		final Iterator<Float> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			arr[i++] = it.next();
+		}
+		return arr;
+	}
+	/**
+	 * convert the collection to double array.
+	 * @param coll the double collection
+	 * @return the double array.
+	 * @since 1.2.0
+	 */
+	public static double[] toDoubleArray(Collection<Double> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final double[] arr = new double[coll.size()];
+		final Iterator<Double> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			arr[i++] = it.next();
+		}
+		return arr;
+	}
+	/**
+	 * convert the collection to boolean array.
+	 * @param coll the boolean collection
+	 * @return the boolean array.
+	 * @since 1.2.0
+	 */
+	public static boolean[] toBooleanArray(Collection<Boolean> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final boolean[] arr = new boolean[coll.size()];
+		final Iterator<Boolean> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			arr[i++] = it.next();
+		}
+		return arr;
+	}
+	/**
+	 * convert the collection to boolean array.
+	 * @param coll the boolean collection
+	 * @return the char array.
+	 * @since 1.2.0
+	 */
+	public static char[] toCharArray(Collection<?> coll){
+		if(coll == null || coll.isEmpty()){
+			return null;
+		}
+		final char[] arr = new char[coll.size()];
+		final Iterator<?> it = coll.iterator();
+		int i = 0 ;
+		for (; it.hasNext();){
+			String s = it.next().toString();
+			try {
+				arr[i] = (char)Integer.parseInt(s);
+			}catch (NumberFormatException e){
+				if(s.length() > 1){
+					throw new UnsupportedOperationException(s);
+				}
+				arr[i] = s.charAt(0);
+			}
+			i++;
+		}
+		return arr;
+	}
+	/**
 	 * convert the int array to a list.
 	 * @param arr the int array
 	 * @return the result list.
